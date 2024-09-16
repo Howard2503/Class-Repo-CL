2fbb1531cd455def0034f7f42f4cb1366ed019c9
Howard2503 <44593350+Howard2503@users.noreply.github.com>
2024-09-16 19:46:44 +0800

    Update sketch.js

--- Imaginary_mailbox_2024_09_11_07_53_04/sketch.js
+++ Imaginary_mailbox_2024_09_11_07_53_04/sketch.js
@@ -1,8 +1,8 @@
 function setup() {
-  createCanvas(windowWidth/2, windowHeight/2);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(map(sin(frameCount / 10), -1, 1, 0, 255), map(cos(frameCount / 10), -1, 1, 0, 255), 255);
-  // print(frameCount);
+  print(frameCount);
 }
